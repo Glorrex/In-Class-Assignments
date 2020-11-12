291f098814b041d0a22d47b8556c5d263a60c49c
Glorrex <zstoneburner@gmail.com>
2020-11-12 17:50:58 -0500

    Changed Ui Text script and Game manager game object

--- Assets/Scripts/UIText.cs
+++ Assets/Scripts/UIText.cs
@@ -5,19 +5,25 @@ using UnityEngine.UI;
 public class UIText : MonoBehaviour
 {
     public Text LivesText;
+    public Text scoreText;
+    public Text coinText;
     private GameObject gameManager;
     private int Lives;
+    private int coins;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
         gameManager = this.gameObject;
-        Lives = gameManager.GetComponent<GameManager>().Lives;
+        Lives = gameManager.GetComponent<GameManager>().getLives();
+        score = gameManager.GetComponent<GameManager>().getScore();
+        coins = gameManager.GetComponent<GameManager>().getCoins();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Lives = gameManager.GetComponent<GameManager>().Lives;
+        Lives = gameManager.GetComponent<GameManager>().getLives();
         LivesText.text = "Lives = " + Lives;
     }
 }
